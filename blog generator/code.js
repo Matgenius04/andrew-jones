715f720512b4729060b3b81c5eadf7faf70d1a5f
F4Tornado <h.rovnyak@gmail.com>
2017-11-11 21:25:43 -0500

    thingerinoepierino

--- blog generator/code.js
+++ blog generator/code.js
@@ -3,11 +3,11 @@ var text;
 var date = new Date();
 date = date.getMonth()+'/'+date.getDate()+'/'+date.getFullYear();
 function generate() {
-  title = document.getElementById('title');
-  text = document.getElementById('text');
+  title = document.getElementById('title').innerHTML;
+  text = document.getElementById('text').innerHTML;
   var doot = "<h2 class='heading'>"+title+"</h2> \
   <date class='date'>"+date+"</date> \
   <p class='normal-text'>"+text+"</p>";
-  document.createElement('p').innerHTML = 
+  document.getElementById('code').innerHTML = doot;
   return doot;
 }
